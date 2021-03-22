7b4c6e76a34ef70e88c74f0936b99b57fc3da848
perdizio <43557422+perdizio@users.noreply.github.com>
2021-03-22 09:14:26 -0300

    Update Triangulo.java

--- Classe/figures/Triangulo.java
+++ Classe/figures/Triangulo.java
@@ -1,1 +1,40 @@
+package figures;
 
+import java.awt.*;
+
+public class Triangulo {
+	 int x, y;
+	 int w, h;
+	 Polygon p = new Polygon();
+
+	public Triangulo (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+
+		//X
+        p.npoints = 3;
+        p.xpoints[0] = x ;
+        p.xpoints[1] = y;
+        p.xpoints[2] = w;
+
+		//Y
+        p.ypoints[0] = y;
+        p.ypoints[1] = w ;
+        p.ypoints[2] = y;
+
+    }
+
+
+	private void print () {
+        System.out.format("Triangulo tamanho: (%d,%d) e posicao: (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+	public void paint (Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+		g2d.drawPolygon(p.xpoints, p.ypoints, p.npoints);
+    }
+} 
