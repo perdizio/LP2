3c3ff0a8c046f7d6dfa92d19267e2b0942367a25
perdizio <43557422+perdizio@users.noreply.github.com>
2021-04-21 15:33:31 -0300

    Update Rect.java

--- Projeto/figures/Rect.java
+++ Projeto/figures/Rect.java
@@ -1,32 +1,48 @@
 package figures;
 
 import java.awt.*;
+import java.awt.event.*;
 
-public class Rect  extends Figure {
-    int x, y;
-    int w, h;
-	Color a;
-	Color b;
-
-    public Rect (int x, int y, int w, int h, Color a, Color b) {
+public class Rect extends Figure {
+	Color cb;
+	//Color corBorda;
+	Rectangle  r;
+    public Rect (int x, int y, int w, int h, Color corBorda, Color cb) {
+		 super(x, y,w,h,corBorda,cb);
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-		this.a=a;
-		this.b=b;
+        this.corBorda = corBorda;
+		this.cb=cb;
+		this.r = new Rectangle(this.x, this.y, this.w, this.h);
+    }
+	
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
     }
 
-
-
-   public void paint (Graphics g) {
-            Graphics2D g2d = (Graphics2D) g;
-            g2d.setColor(this.a);
-            g2d.drawRect(this.x+1 , this.y+1, this.w-1, this.h-1);
-            g2d.setColor(this.b);
-            g2d.fillRect((this.x-1), (this.y-1), (this.w+1), (this.h+1)); 
-            g2d.setColor(this.a); 
-        
-
+    public void paint (Graphics g) {
+       Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(this.corBorda);
+        g2d.draw(this.r);
+        g2d.setColor(this.cb);
+        g2d.fillRect(this.x,this.y, this.w,this.h);
+    }
+	
+	
+	 public boolean contains(MouseEvent evt) {
+        if (this.r.contains(evt.getPoint()))
+            return true;
+        return false;
+    }
+	
+	public void drag(int x,int y, Point p){
+        this.x+=x;
+        this.y+=y;
+        this.r = new Rectangle(this.x,this.y, this.w,this.h);
+    }
 }
-}
+
