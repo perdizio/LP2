496cf5ff5f69eca43d49f2498207312037a23c9f
perdizio <43557422+perdizio@users.noreply.github.com>
2021-05-16 16:18:38 -0300

    Create IVisible.java

---
+++ Projeto/ivisible/IVisible.java
@@ -0,0 +1,8 @@
+package ivisible;
+import java.awt.Graphics;
+import java.awt.event.*;
+public interface Ivisible{
+    public boolean clicked(MouseEvent evt);
+    public void    paint   (Graphics g);
+
+}
